3f8214164737910ca92249862539f3458662d937
Konrad Nawara <60048431+Ech4le@users.noreply.github.com>
2023-02-10 08:06:01 +0000

    crappy designed landing and login works

--- src/components/SimpleNav.jsx
+++ src/components/SimpleNav.jsx
@@ -1,24 +1,28 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import HealingIcon from '@mui/icons-material/Healing';
+import HealingIcon from "@mui/icons-material/Healing";
+import { Typography } from "@mui/material";
 
 export default function SimpleNav() {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}>
-						<HealingIcon />
-					</IconButton>
-				</Toolbar>
+			<AppBar
+				position="static"
+				style={{ background: "transparent", boxShadow: "none" }}>
+				<IconButton
+					disabled
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="menu"
+					sx={{ mr: 2 }}>
+					<HealingIcon />
+					<Typography fontSize={14} ml={1}>
+						Trauma
+					</Typography>
+				</IconButton>
 			</AppBar>
 		</Box>
 	);
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,11 +1,39 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import SimpleNav from "../components/SimpleNav";
+import { withStyles } from "@mui/styles";
+import Copyright from "../components/Copyright";
+import Landing1 from "../assets/Landing1.jpg";
+import { Link } from "react-router-dom";
+
+const DescriptionText = withStyles({
+	root: {
+		color: "#808080",
+	},
+})(Typography);
 
 const LandingPage = () => {
 	return (
 		<Box>
 			<SimpleNav />
-			<Typography>You pay, we cure.</Typography>
+			<Box component="main" mt={2} ml={2} mr={2}>
+				<Typography variant="h4" mb={2} fontSize={30}>
+					You pay, we cure.
+				</Typography>
+				<DescriptionText mb={2}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Praesent in tellus efficitur, tincidunt augue a, porttitor
+					nunc. Sed suscipit neque pellentesque maximus blandit.
+				</DescriptionText>
+				<Button component={Link} to="/login" variant="contained">Sign In</Button>
+				<br />
+				<Box
+					mt={1}
+					sx={{ width: "100%" }}
+					component="img"
+					src={Landing1}
+				/>
+				<Copyright sx={{ mt: 4, mb: 4 }} />
+			</Box>
 		</Box>
 	);
 };
